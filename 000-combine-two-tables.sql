104d1dec3567c5224891364269dc5cdaa4a14233
Lyndon Tuala <lyndontuala28@gmail.com>
2025-08-16 21:51:20 +0800

    git init - D1 progress (1p,1s)

---
+++ 000-combine-two-tables.sql
@@ -0,0 +1,9 @@
+-- https://leetcode.com/problems/combine-two-tables/
+select 
+    Person.firstName,
+    Person.lastName,
+    Address.city,
+    Address.state
+from
+    Person
+    left join Address on Person.personId = Address.personId;
